9c38b35008db831c4a5d794f9fb8099535eb5f7d
Valdid <valentin-didomenico@orange.fr>
2022-07-17 19:18:58 +0200

    Music fix

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -7,18 +7,45 @@ public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
 
-    [SerializeField] private AudioEvent m_menuMusic;
     [SerializeField] private AudioEvent m_mainMusic;
     [SerializeField] private AudioEvent m_bulletWoodImpact;
+
+    private bool m_isMusicPlaying;
 
     private void OnEnable()
     {
         Bullet.OnHitObject += Bullet_OnHitObject;
+        SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+    }
+
+    private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
+    {
+        if (SceneManager.GetActiveScene().name == "SampleScene")
+        {
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName("MusicStates", 0);
+            float value;
+            FMODUnity.RuntimeManager.StudioSystem.getParameterByName("MusicStates", out value);
+            Debug.Log($"Music state = {value}");
+        }
+        else if (SceneManager.GetActiveScene().name == "TitleScreen")
+        {
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName("MusicStates", 6);
+            float value;
+            FMODUnity.RuntimeManager.StudioSystem.getParameterByName("MusicStates", out value);
+            Debug.Log($"Music state = {value}");
+        }
+        if (!m_isMusicPlaying)
+        {
+            Debug.Log("Playing music");
+            m_isMusicPlaying = true;
+            m_mainMusic.Play(gameObject);
+        }
     }
 
     private void OnDisable()
     {
         Bullet.OnHitObject -= Bullet_OnHitObject;
+        SceneManager.sceneLoaded -= SceneManager_sceneLoaded;
     }
 
     private void Bullet_OnHitObject(Bullet _bullet, GameObject _hit, Bullet.ContactType _contactType)
@@ -34,8 +61,10 @@ public class AudioManager : MonoBehaviour
 
     private void Awake()
     {
-        if (instance)
-            Destroy(this);
+        m_isMusicPlaying = false;
+
+        if (instance != null)
+            Destroy(gameObject);
         else
         {
             instance = this;
@@ -45,29 +74,23 @@ public class AudioManager : MonoBehaviour
 
     private void OnLevelWasLoaded(int level)
     {
+        /*
+        Debug.Log("ALLO");
         if (SceneManager.GetActiveScene().name == "SampleScene")
         {
-            m_menuMusic.Stop();
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName("MusicStates", 0);
+        }
+        else if (SceneManager.GetActiveScene().name == "TitleScreen")
+        {
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName("MusicStates", 6);
+        }
+        if (!m_isMusicPlaying)
+        {
+            Debug.Log("Playing music");
+            m_isMusicPlaying = true;
             m_mainMusic.Play(gameObject);
         }
-        else
-        {
-            m_mainMusic.Stop();
-            m_menuMusic.Play(gameObject);
-        }
-    }
-
-    private void Start()
-    {
-        Debug.Log(SceneManager.GetActiveScene().name);
-        if (SceneManager.GetActiveScene().name == "SampleScene")
-        {
-            m_mainMusic.Play(gameObject);
-        }
-        else
-        {
-            m_menuMusic.Play(gameObject);
-        }
+        */
     }
 
     private IEnumerator DestroyCooldown(GameObject _emitter)
