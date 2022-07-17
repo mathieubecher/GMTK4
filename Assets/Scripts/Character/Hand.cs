dd83f5a3e23e5526c5c6c5431a1b72d3be0e81ee
Becher <mbelkhenchir@gmail.com>
2022-07-17 15:42:02 +0200

    Anim hand

--- Assets/Scripts/Character/Hand.cs
+++ Assets/Scripts/Character/Hand.cs
@@ -8,15 +8,20 @@ public class Hand : MonoBehaviour
     [SerializeField] private Character m_character;
     [SerializeField] private SpriteRenderer m_sprite;
     [SerializeField] private float m_distance = 0.6f;
-    // Start is called before the first frame update
-    
+
+    private Animator m_animator;
     void OnEnable()
     {
+        m_character.OnShoot += Shoot;
+        m_character.OnReload += Reload;
         m_character.OnDead += Dead;
+        m_animator = m_sprite.transform.parent.GetComponent<Animator>();
     }
 
     void OnDisable()
     {
+        m_character.OnShoot -= Shoot;
+        m_character.OnReload -= Reload;
         m_character.OnDead -= Dead;
     }
 
@@ -24,12 +29,23 @@ public class Hand : MonoBehaviour
     {
         gameObject.SetActive(false);
     }
+
+    void Shoot(int _bullet)
+    {
+        m_animator.SetTrigger("Shoot");
+    }
+
+    void Reload()
+    {
+        m_animator.SetTrigger("Reload");
+        
+    }
     // Update is called once per frame
     void Update()
     {
         transform.position = (Vector2) m_character.transform.position + m_character.targetDir * m_distance; 
         
-        m_sprite.transform.eulerAngles = new Vector3(0f,0f, Vector2.SignedAngle(Vector2.right, m_character.targetDir));
+        m_sprite.transform.parent.transform.eulerAngles = new Vector3(0f,0f, Vector2.SignedAngle(Vector2.right, m_character.targetDir));
         m_sprite.flipY = m_character.targetDir.x < 0;
     }
 }
